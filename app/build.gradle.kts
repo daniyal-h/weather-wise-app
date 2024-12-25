1239c570ed28abdfb9ee6739e7b4b96d85ccbf22
Daniyal <hasnaind@myumanitoba.ca>
2024-12-25 01:49:42 -0600

    Added new draft layout with icons and improved UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("com.airbnb.android:lottie:5.2.0")
     implementation("org.json:json:20230618")
     implementation("com.android.volley:volley:1.2.1")
     testImplementation("junit:junit:4.13.2")
