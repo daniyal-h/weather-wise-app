c003510b3ef2bdac6f4bfea3d87d590547dcb5f6
Daniyal <hasnaind@myumanitoba.ca>
2024-12-24 23:28:31 -0600

    added themes and font; altered UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("org.json:json:20230618")
     implementation("com.android.volley:volley:1.2.1")
     testImplementation("junit:junit:4.13.2")
